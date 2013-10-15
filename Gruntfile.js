fac20b5b8fc4a9df6eed5c56b381f651b9ddc556
Pavel Strashkin <pavel.strashkin@nexenta.com>
2013-10-15 10:42:03 -0700

    Review Gruntfile.js and package.json
        - fix indentation
        - sort depedencies and tasks
        - remote server and watch tasks (and related dependencies)
          as we do not need those
        - use expand to compile CS to JS instead of bothering ourselves with
          files names

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,59 +1,35 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
     coffee: {
-      compile: {
-          files: {
-            'lib/composer.js': 'src/composer.coffee',
-            'lib/construct.js': 'src/construct.coffee',
-            'lib/errors.js': 'src/errors.coffee',
-            'lib/events.js': 'src/events.coffee',
-            'lib/loader.js': 'src/loader.coffee',
-            'lib/nodes.js': 'src/nodes.coffee',
-            'lib/parser.js': 'src/parser.coffee',
-            'lib/reader.js': 'src/reader.coffee',
-            'lib/resolver.js': 'src/resolver.coffee',
-            'lib/scanner.js': 'src/scanner.coffee',
-            'lib/validator.js': 'src/validator.coffee',
-            'lib/tokens.js': 'src/tokens.coffee',
-            'lib/util.js': 'src/util.coffee',
-            'lib/traits.js': 'src/traits.coffee',
-            'lib/resourceTypes.js': 'src/resourceTypes.coffee',
-            'lib/schemas.js': 'src/schemas.coffee',
-            'lib/protocols.js': 'src/protocols.coffee',
-            'lib/securitySchemes.js': 'src/securitySchemes.coffee',
-            'lib/transformations.js': 'src/transformations.coffee',
-            'lib/joiner.js': 'src/joiner.coffee',
-            'lib/raml.js': 'src/raml.coffee'
-          }
-        }
+      files: {
+        expand: true,
+        flatten: true,
+        src: 'src/*.coffee',
+        dest: 'lib',
+        ext: '.js'
+      }
     },
+
     browserify: {
       'dist/raml-parser.js': ['src/browserify.js', 'lib/*.js', 'node_modules/q/q.js']
     },
-    watch: {
-      coffee: {
-        files: ['src/*.coffee'],
-        tasks: ['coffee']
-      },
-      javascript: {
-        files: ['lib/*.js', 'src/browserify.js'],
-        tasks: ['browserify']
-      }
-    },
+
     uglify: {
       options: {
         mangle: {
           except: ['RAML', 'Q']
         }
       },
+
       javascript: {
         files: {
           'dist/raml-parser.min.js': ['dist/raml-parser.js']
         }
       }
     },
+
     connect: {
       server: {
         options: {
@@ -62,46 +38,48 @@ module.exports = function(grunt) {
         }
       }
     },
+
     mocha_phantomjs: {
       all: {
         options: {
           urls: [
             'http://localhost:9001/test/specs/parser.html'
           ],
-        reporter: 'dot'
+          reporter: 'dot'
         }
       }
     },
+
     mochacli: {
-        options: {
-            require: ['chai', 'chai-as-promised'],
-            reporter: 'dot',
-            bail: true
-        },
-        all: ['test/specs/*.js']
+      options: {
+        require: ['chai', 'chai-as-promised'],
+        reporter: 'dot',
+        bail: true
+      },
+      all: ['test/specs/*.js']
     },
+
     coffeelint: {
       app: ['src/*.coffee'],
       options: {
-        'max_line_length': {
-          'level': 'ignore'
+        max_line_length: {
+          level: 'ignore'
         }
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-browserify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-mocha-phantomjs');
+  grunt.loadNpmTasks('grunt-coffeelint');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-mocha-cli');
-  grunt.loadNpmTasks('grunt-coffeelint');
+  grunt.loadNpmTasks('grunt-mocha-phantomjs');
 
   grunt.registerTask('compile', ['coffeelint', 'coffee']);
-  grunt.registerTask('default', ['coffeelint', 'coffee', 'browserify', 'uglify']);
   grunt.registerTask('test', ['default', 'connect', 'mochacli', 'mocha_phantomjs']);
   grunt.registerTask('test-ui-only', ['coffee', 'browserify', 'uglify', 'connect', 'mocha_phantomjs']);
-  grunt.registerTask('server', ['default', 'connect', 'watch']);
+
+  grunt.registerTask('default', ['coffeelint', 'coffee', 'browserify', 'uglify']);
 };
